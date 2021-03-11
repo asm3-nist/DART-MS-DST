1ba842fe240ce5db5dc0917b1d55dd5b9c540662
A.S. Moorthy <asm3@nist.gov>
2021-03-11 12:14:48 -0500

    Add files via upload

--- shiny/source/GUI/asm_GUI_DBV.R
+++ shiny/source/GUI/asm_GUI_DBV.R
@@ -10,7 +10,8 @@ asm_GUI_DBV <- tabPanel(
        div(selectInput("RefLibSelect","Reference Library: ",choices=Libraries)),
        div(DT::dataTableOutput("Library"), style = "font-size:70%"),
        div(strong(p("References")),
-           p("Sisco et. al. (accepted). Creation and Release of an Updated NIST DART-MS Forensics Database."),
+           p(HTML("Sisco et. al.<a href='https://pubs.acs.org/doi/abs/10.1021/jasms.0c00416' target='_blank'>
+                   J. Am. Soc. Mass Spectrom. 2021, 32, 3, 685–689.</a>")),
               style="color:black;font-size:8pt")
    ),
 
